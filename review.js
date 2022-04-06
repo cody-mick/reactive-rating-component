d947a291451c0186f1da6bef0d6e72f8079ea62f
cody-mick <cmick@byui.edu>
2022-04-06 16:54:57 -0600

    finished I think?

--- review.js
+++ review.js
@@ -2,18 +2,25 @@ let ratingBox = document.querySelector(".rating-box");
 let thankYouBox = document.querySelector(".thank-you");
 let submitBtn = document.querySelector(".rating-submit");
 
-submitBtn.addEventListener("click", function(event){
-    ratingBox.classList.add("hidden");
-    thankYouBox.classList.remove("hidden");
+submitBtn.addEventListener("click", function (event) {
+	ratingBox.classList.add("hidden");
+	thankYouBox.classList.remove("hidden");
 });
 
-let ratings = document.querySelectorAll(".number");
-ratings.forEach(rating => {
-    rating.addEventListener("click", function(event) {
-        if (rating.classList.contains("selected")) {
-            rating.classList.remove("selected");
-        } else {
-            rating.classList.add("selected");
-        };
-    })
-});
+let ratings = [...document.querySelectorAll(".number")];
+let selectedItem;
+
+for (const rating of ratings) {
+	rating.addEventListener("click", (e) => {
+		selectedItem && selectedItem.classList.remove("selected");
+		selectedItem = e.target;
+		e.target.classList.add("selected");
+	});
+}
+
+// Get rating number and pass into the DOM for display on the thank you page
+function getRating() {
+	let rating = document.querySelector(".selected").textContent;
+	let ratingDisplay = document.querySelector(".rating");
+	ratingDisplay.textContent = " " + rating + " ";
+}
